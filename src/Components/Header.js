6013bb757dbbdef69c3811e514163214584584ab
ikhuohoneboreimethankgod@gmail.com <calebikhuohon@gmail.com>
2020-11-30 20:15:08 +0100

    Incoporated the back button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,27 +3,50 @@ import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Link, useHistory } from 'react-router-dom';
 
-const Header = () => {
+//useHisotry give us the browsers history
+
+        //object destructuring
+const Header = ({ backButton }) => {
+
+    const history = useHistory();
+
     return (
         <div className="Header" >
+            {backButton ? (
+               <IconButton onClick={() => history.replace(backButton)}>
+                <ArrowBackIosIcon fontSize="large" className="header__back"/>
+               </IconButton>
+            ) : (
+                    <IconButton>
+                        <PersonIcon
+                            fontSize="large"
+                            className="header__icon"
+                        />
+                    </IconButton>
+                )}
             <IconButton>
-                <PersonIcon
-                    fontSize="large"
-                    className="header__icon"
-                />
+                <Link to="/">
+                    <img
+                        className="header__logo"
+                        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                        alt="tinder-logo"
+                    />
+
+                </Link>
+
             </IconButton>
-            <img
-                className="header__logo"
-                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
-                alt="tinder-logo"
-            />
-            <IconButton>
-                <ForumIcon
-                    fontSize="large"
-                    className="header__icon"
-                />
-            </IconButton>
+            <Link to="/chat">
+                <IconButton>
+                    <ForumIcon
+                        fontSize="large"
+                        className="header__icon"
+                    />
+                </IconButton>
+
+            </Link>
         </div>
     )
 }
